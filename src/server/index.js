71e1b7c0529b501003b2e7dac1b288fc257c59cb
fdAssis <fd07ass@gmail.com>
2021-02-28 21:12:01 -0300

    pages: index, about

--- src/server/index.js
+++ src/server/index.js
@@ -6,8 +6,20 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 
+let mascots = [
+  { name: 'Sammy', organization: 'DigitalOcean', birth_year: 2012 },
+  { name: 'Tux', organization: 'Linux', birth_year: 1996 },
+  { name: 'Moby Dock', organization: 'Docker', birth_year: 2013 },
+];
+
+let tagline =
+  'No programming concept is complete without a cute animal mascot.';
+
 app.get('/', (request, response) => {
-  response.render('../src/pages/index');
+  response.render('../src/pages/index', {
+    mascots,
+    tagline,
+  });
 });
 
 app.get('/about', (request, response) => {
