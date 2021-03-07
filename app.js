be322f335b9904810f941b4c9e7bc8c35e72be1c
fdAssis <fd07ass@gmail.com>
2021-03-07 18:01:30 -0300

    update project

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const port = 3000;
 
 // Static Files
 app.use(express.static('src'));
-app.use('/css', express.static(__dirname + 'src/public/css'));
+app.use('/css', express.static(__dirname + '/src/public/css'));
 
 // Set View
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-  response.render('src/views/index');
+  response.render('index');
 });
 
 app.listen(port, () => {
